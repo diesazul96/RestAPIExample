d2600dc2aecdd102f7309abb58901db883f5e81f
fabian.giraldo <fabian.giraldo@fgiraldo.usa.loc>
2019-04-04 11:42:59 -0500

    Initial Commit

---
+++ src/main/java/com/mkyong/rest/EquipoService.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mkyong.rest;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author DALVAREZ
+ */
+@Path("/equipo")
+public class EquipoService {
+    
+}
